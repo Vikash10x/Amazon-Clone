36c1ed73214f11668245bd186fab654c856cdd78
Vikash Kumawat <vvikash0012@gmail.com>
2025-05-19 14:13:43 +0530

    data.js

---
+++ src/components/data.js
@@ -0,0 +1,50 @@
+export const cardData = [
+  {
+    titel: "Love Birds Wall Art",
+    src: "public/Images/box1-img.jpg",
+  },
+  {
+    titel: "Shop Laptops & Tablets",
+    src: "public/Images/box2-img.jpg",
+  },
+  {
+    titel: "Maroon Casual Shirt",
+    src: "public/Images/box3-img.jpg",
+  },
+  {
+    titel: "Mobile phones",
+    src: "public/Images/box4-img.webp",
+  },
+  {
+    titel: "Grill Microwave",
+    src: "public/Images/box5-img.jpg",
+  },
+  {
+    titel: "Park Avenue Perfume",
+    src: "public/Images/box6-img.jpg",
+  },
+  {
+    titel: "Beauty Flatlay",
+    src: "public/Images/box7-img.jpg",
+  },
+  {
+    titel: "Nike Footwear Sale",
+    src: "public/Images/box8-img.jpg",
+  },
+  {
+    titel: "Elegant Jewelry Display",
+    src: "public/Images/box9-img.jpg",
+  },
+  {
+    titel: "Fresh Juice Promo",
+    src: "public/Images/box10-img.jpg",
+  },
+  {
+    titel: "ClearTime",
+    src: "public/Images/box11-img.webp",
+  },
+  {
+    titel: "SoundMax",
+    src: "public/Images/box12-img.jpg",
+  },
+];
