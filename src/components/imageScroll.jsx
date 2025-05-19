f0d092a78c1857a8d760466b68dbf9fb84b14539
Vikash Kumawat <vvikash0012@gmail.com>
2025-05-19 15:21:39 +0530

    ImageScroll.jsx

---
+++ src/components/imageScroll.jsx
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+
+function Imagescroll() {
+  const sliderRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollLeft -= sliderRef.current.clientWidth;
+    }
+  };
+
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollLeft += sliderRef.current.clientWidth;
+    }
+  };
+
+  const images = [
+    "public/Images/scroll-image2.jpg",
+    "public/Images/scroll-image1.jpg",
+    "public/Images/scroll-image3.jpg",
+    "public/Images/scroll-image4.jpg",
+    "public/Images/scroll-image5.jpg",
+    "public/Images/scroll-image6.jpg",
+  ];
+
+  return (
+    <div className="slider-wrapper">
+      <button className="nav left" onClick={scrollLeft}>
+        &lt;
+      </button>
+      <div className="slider" ref={sliderRef}>
+        {images.map((src, index) => (
+          <img key={index} src={src} alt={`slide-${index}`} className="slide" />
+        ))}
+      </div>
+
+      <button className="nav right" onClick={scrollRight}>
+        &gt;
+      </button>
+    </div>
+  );
+}
+
+export default Imagescroll;
